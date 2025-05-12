58ef7456f17cf443f079093018b782ba1d1e9ebb
Asray Gopa <69517250+asrayg@users.noreply.github.com>
2025-05-11 19:42:55 -0500

    CORS helper

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ let dbClient;
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+  origin: ['http://localhost:5174', 'http://127.0.0.1:5174', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'UserId', 'userid']
